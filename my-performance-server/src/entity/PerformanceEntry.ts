da457e2918627ff5c6b5bba56c1c048e76df2820
Guilherme Iscaro <cabelitostos@gmail.com>
2019-11-02 14:00:41 -0300

    feat(fixtures): add fixtures
    This commit adds some fixtures to be used as test data. It alsos
    downgrade the postgres version since typeorm has a bug which
    can't handles dates on postgres 12.

---
+++ my-performance-server/src/entity/PerformanceEntry.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  Index,
+} from 'typeorm';
+
+@Entity()
+export default class PerformanceEntry extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Index()
+  @Column('text')
+  userId: string;
+
+  @Column('int')
+  distance: number;
+
+  @Column('int')
+  energy: number;
+
+  @Column('int')
+  calories: number;
+
+  @Column({ type: 'timestamp with time zone', unique: true })
+  date: Date;
+}
