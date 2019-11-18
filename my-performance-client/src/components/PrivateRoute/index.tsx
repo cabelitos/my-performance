0bd0f16f3dd5555337ae4916f453b0d2eb7f8327
Guilherme Iscaro <cabelitostos@gmail.com>
2019-11-18 15:08:47 -0300

    feat(client/PrivateRoute): add loading
    Add a loading spinner while the auth process is running. This will
    help the user to know that the app is not frozen.

--- my-performance-client/src/components/PrivateRoute/index.tsx
+++ my-performance-client/src/components/PrivateRoute/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Route, RouteProps, Redirect } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { useAuth } from '../../auth';
 import routeNames from '../../routes/routeNames';
+import FullScreenLoader from '../FullScreenLoader';
+
+const useStyles = makeStyles(() => ({
+  container: {
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
 
 const PrivateRoute = ({
   component: Component,
@@ -10,18 +22,22 @@ const PrivateRoute = ({
   ...rest
 }: RouteProps): JSX.Element => {
   const { isAuthenticated, loading } = useAuth();
-
+  const styles = useStyles();
   const onRender = React.useCallback(
     (props): JSX.Element | null => {
       if (loading) {
-        return null;
+        return (
+          <div className={styles.container}>
+            <FullScreenLoader />
+          </div>
+        );
       }
       if (isAuthenticated && Component) {
         return <Component {...props} />;
       }
       return <Redirect to={routeNames.home} />;
     },
-    [isAuthenticated, loading, Component],
+    [isAuthenticated, loading, Component, styles],
   );
 
   return <Route path={path} render={onRender} {...rest} />;
